3f6b4bb2a67e8fa53a075f496938d6d78b1933c1
Darren Chan Yu Hao <darren_chan@mymail.sutd.edu.sg>
2024-12-07 21:47:19 +0800

    feat - completed MiniGameManager Script

--- Assets/Scripts/MiniGameManager.cs
+++ Assets/Scripts/MiniGameManager.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class MiniGameManager : MonoBehaviour
 {
-    class Question
+    public class Question
     {
         public string CorrectAnswer;
-        public string PlayerAnswer;
         public string QuestionClue;
 
         public Question(string CorrectAnswer, string QuestionClue)
@@ -18,14 +18,50 @@ public class MiniGameManager : MonoBehaviour
         }
     }
 
-    [SerializeField]
-    int maxQuestions = 5;
-    [SerializeField, Tooltip("Duration per question in seconds.")]
-    float quizDurationPerQuestion = 100;
+    //[SerializeField]
+    //int maxQuestions = 5;
+    [SerializeField, Tooltip("Time limit for the quiz in seconds.")]
+    float quizDuration = 600;
+
+    [SerializeField, Tooltip("Max point per question")]
+    public int itemScore = 100;
+
+    [SerializeField, Tooltip("Grace Period for the question in seconds.")]
+    public int gracePeriod = 10;
+
+    [SerializeField, Tooltip("Score reduction per second after grace period.")]
+    public int scoreReductionPerSecond = 3;
+
+    [SerializeField, Tooltip("Minimum score per question.")]
+    public int minScore = 20;
+
+    [Header("Events")]
+
+    [SerializeField, Tooltip("Invoked when the quiz moves to the next question.")]
+    public UnityEvent OnNextQuestion = new();
+
+    [SerializeField, Tooltip("Invoked when the quiz is completed.")]
+    public UnityEvent OnQuizCompleted = new();
+
+    /// <summary>
+    /// Currently active question.
+    /// </summary>
+    public int currentQuestion { get; private set; } = 0;
+    /// <summary>
+    /// Total score of the quiz.
+    /// </summary>
+    public int totalScore { get; private set; } = 0;
+    /// <summary>
+    /// Timer for the current question.
+    /// </summary>
+    public float timerCurrentQuestion { get; private set; } = 0;
+    /// <summary>
+    /// Total time elapsed since the quiz started.
+    /// </summary>
+    public float timerTotal { get; private set; } = 0;
 
     MuseumLabel[] museumLabels;
     List<Question> questionList = new();
-    int currentQuestion = 0;
     bool quizRunning = false;
 
     private void Start()
@@ -35,6 +71,15 @@ public class MiniGameManager : MonoBehaviour
         {
             museumLabel.artefactInteractable.firstSelectEntered.AddListener((eventArgs) =>
                 OnArtefactSelected(museumLabel.artefactInteractable, museumLabel.artefactDescription.Title));
+        }
+    }
+
+    private void Update()
+    {
+        if (quizRunning)
+        {
+            timerCurrentQuestion += Time.deltaTime;
+            timerTotal += Time.deltaTime;
         }
     }
 
@@ -49,12 +94,16 @@ public class MiniGameManager : MonoBehaviour
             questionList.Add(new Question(arteDesc.Title, arteDesc.MiniGameDescription));
         }
         currentQuestion = 0;
+        totalScore = 0;
+        timerCurrentQuestion = 0;
+        timerTotal = 0;
         DisplayQuestion(currentQuestion);
     }
 
     public void NextQuestion()
     {
         DisplayQuestion(currentQuestion++);
+        timerCurrentQuestion = 0;
     }
 
     private void DisplayQuestion(int currentQuestion)
@@ -66,17 +115,21 @@ public class MiniGameManager : MonoBehaviour
         }
 
         // Do some shit
-    }
-
-    private void AnswerQuestion(string answer)
-    {
-        questionList[currentQuestion].PlayerAnswer = answer;
-        DisplayQuestion(currentQuestion);
+        OnNextQuestion.Invoke();
     }
 
     private void DisplayResults()
     {
+        OnQuizCompleted.Invoke();
+    }
 
+    private void AnswerQuestion(string answer)
+    {
+        if(questionList[currentQuestion].CorrectAnswer == answer)
+        {
+            totalScore += CalculateScore(timerCurrentQuestion);
+            NextQuestion();
+        }
     }
 
     private int[] GenerateQuestionIndices()
@@ -88,10 +141,7 @@ public class MiniGameManager : MonoBehaviour
         System.Random rng = new System.Random();
         rng.Shuffle(arr);
 
-        int totalQuestions = arr.Count() < maxQuestions ? arr.Count() : maxQuestions;
-        int[] quizIndices = arr[0..totalQuestions];
-
-        return quizIndices;
+        return arr;
     }
 
     private void OnArtefactSelected(XRGrabInteractable interactable, string title)
@@ -100,6 +150,17 @@ public class MiniGameManager : MonoBehaviour
             return;
 
         AnswerQuestion(title);
+    }
+
+    private int CalculateScore(float timerCurrentQuestion)
+    {
+        // Question start, grace period, reduce score until min score.
+        if (timerCurrentQuestion <= gracePeriod)
+            return itemScore;
+
+        int score = itemScore - (int)((timerCurrentQuestion - gracePeriod) * scoreReductionPerSecond);
+
+        return score < minScore ? minScore : score;
     }
 }
 
