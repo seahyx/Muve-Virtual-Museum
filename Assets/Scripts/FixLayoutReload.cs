8f5031a683977f943554795107a99ba77006d237
Seah Ying Xiang <seahyx@gmail.com>
2024-12-11 17:26:19 +0800

    Updated clipboard

--- Assets/Scripts/FixLayoutReload.cs
+++ Assets/Scripts/FixLayoutReload.cs
@@ -1,16 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(LayoutGroup))]
 public class FixLayoutReload : MonoBehaviour
 {
-	private LayoutGroup layoutGroup;
-
-	private void Start()
+	private void OnEnable()
 	{
-		layoutGroup = GetComponent<LayoutGroup>();
-		
+		LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
 	}
 }
